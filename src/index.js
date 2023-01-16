cc670c3fffece5dc8511d42ee422e0b9a3953165
lipkij <lipkij@gmail.com>
2023-01-17 01:01:14 +0400

    solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,34 @@
 module.exports = function check(str, bracketsConfig) {
   // your solution
+  stack = [];
+  hash = {};
+  brackets = new Set();
+
+  for (i of bracketsConfig) {
+    hash[i[0]] = i[1];
+    brackets.add(i[0]);
+    brackets.add(i[1]);
+  }
+
+  for (s of str) {
+    if (brackets.has(s)) {
+      if (hash[s]) {
+        if (hash[s] === s) {
+          if (s === stack[stack.length - 1]) {
+            stack.pop();
+          } else {
+            stack.push(hash[s]);
+          }
+        } else {
+          stack.push(hash[s]);
+        }
+      } else if (s === stack[stack.length - 1]) {
+        stack.pop();
+      } else {
+        return false;
+      }
+    }
+  }
+  
+  return stack.length === 0;
 }
